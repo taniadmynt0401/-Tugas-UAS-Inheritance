40d7bc4d45b44c4574912cae1ee23a2372454a38
Tania Damayanti <127859384+taniadmynt0401@users.noreply.github.com>
2023-07-15 13:41:44 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿
+using Inheritance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketBookingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Meminta pengguna untuk memasukkan informasi pemesanan tiket
+            Console.Write("Masukkan Nama: ");
+            string name = Console.ReadLine();
+            Console.Write("Masukkan Nomor Kartu Vaksin: ");
+            string vaccinationCardNumber = Console.ReadLine();
+            Console.Write("Masukkan NIK: ");
+            string nik = Console.ReadLine();
+            Console.Write("Masukkan Nomor Telepon: ");
+            string phoneNumber = Console.ReadLine();
+            Console.Write("Masukkan Nomor Kartu: ");
+            string cardNumber = Console.ReadLine();
+            Console.Write("Masukkan Alamat Rumah: ");
+            string homeAddress = Console.ReadLine();
+
+            // Membuat objek TicketBooking dan mengatur nilai properti menggunakan konstruktor
+            TicketBooking ticketBooking = new TicketBooking(name, vaccinationCardNumber, nik, phoneNumber, cardNumber, homeAddress);
+
+            // Memanggil method BookTicket untuk memesan tiket
+            ticketBooking.BookTicket();
+        }
+    }
+}
---
+++ TicketBooking.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class TicketBooking
+    {
+            // Variabel private
+            private string name;
+            private string vaccinationCardNumber;
+            private string nik;
+            private string phoneNumber;
+            private string cardNumber;
+            private string homeAddress;
+
+            // Konstruktor untuk inisialisasi properti
+            public TicketBooking(string name, string vaccinationCardNumber, string nik, string phoneNumber, string cardNumber, string homeAddress)
+            {
+                this.name = name;
+                this.vaccinationCardNumber = vaccinationCardNumber;
+                this.nik = nik;
+                this.phoneNumber = phoneNumber;
+                this.cardNumber = cardNumber;
+                this.homeAddress = homeAddress;
+            }
+        // Method untuk memesan tiket dan menampilkan informasi pemesanan
+        public void BookTicket()
+        {
+            Console.WriteLine("Tiket berhasil dipesan untuk:");
+            Console.WriteLine("Nama: " + name);
+            Console.WriteLine("Nomor Kartu Vaksin: " + vaccinationCardNumber);
+            Console.WriteLine("NIK: " + nik);
+            Console.WriteLine("Nomor Telepon: " + phoneNumber);
+            Console.WriteLine("Nomor Kartu: " + cardNumber);
+            Console.WriteLine("Alamat Rumah: " + homeAddress);
+        }
+    }
+}
+
+
+
